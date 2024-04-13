33ba488e4f2600f9caa70cae8160fb378435467c
eal <eal@cock.li>
2024-04-13 18:23:45 +0300

    Almost fighting

--- script/FatMan.gd
+++ script/FatMan.gd
@@ -3,13 +3,20 @@ extends RigidBody2D
 enum {STAGED, MOVING, FIGHTING}
 
 var state: = STAGED # essentially paused, the characters spawn first and then run at each other
+@export var facing_right = true
 var direction = Vector2.RIGHT
+@onready var impulse_cd = $ImpulseCooldown
 
 func _ready():
-    pass # Replace with function body.
+    if not facing_right:
+        direction = Vector2.LEFT
+    else:
+        $Sprite2D.flip_h = true
 
 func unstage():
     state = MOVING
+
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,3 +28,7 @@ func _process(delta):
         print("not implemented")
         # do something else
 
+
+
+func _on_impulse_cooldown_timeout():
+    apply_central_impulse(direction)
